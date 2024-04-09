1f77e3d6425149c0690879e90d9778dbc2f9b7f5
enesozmus <ensozmus@gmail.com>
2024-04-09 14:17:09 +0300

    Building a stack of cards

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,9 +7,38 @@
 
 import SwiftUI
 
+// → The only complex part of our next code is how we position the cards inside the card stack so they have slight overlapping.
+// → In this case we’re going to create a new stacked() modifier that takes a position in an array along with the total size of the array, and offsets a view by some amount based on those values.
+// → This will allow us to create an attractive card stack where each card is a little further down the screen than the ones before it.
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
+    // → Now that we’ve designed one card and its associated card view, the next step is to build a stack of those cards to represent the things our user is trying to learn.
+    // → This stack will change as the app is used because the user will be able to remove cards, so we need to mark it with @State.
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View {
-        CardView(card: .example)
+        // → Around that VStack will be another ZStack, so we can place our cards and timer on top of a background.
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            // → Around that ZStack will be a VStack. Right now that VStack won’t do much, but later on it will allow us to place a timer above our cards.
+            VStack {
+                // → Our stack of cards will be placed inside a ZStack so we can make them partially overlap with a neat 3D effect.
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
